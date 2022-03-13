3e6a90c37f875fe192e620a9c600a75022fdfd2f
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2022-03-13 18:48:51 -0300

    atualizado o tipo do IUserRepository

--- src/modules/userSample/database/repositoriesInterface/IUserRepository.ts
+++ src/modules/userSample/database/repositoriesInterface/IUserRepository.ts
@@ -4,7 +4,7 @@ import User from '../typeorm/entities/User';
 export default interface IUserRepository {
   create(data: ICreateUserDTO): Promise<User>;
   list(): Promise<User[]>;
-  find(id: Number): Promise<User>;
+  find(id: string): Promise<User>;
   remove(id: any): void;
   update(data: IUpdateUserDTO): Promise<User>;
   findByEmail(email: string): Promise<User>;
